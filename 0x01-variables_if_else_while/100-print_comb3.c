8d1e9789550b7ce8c330718a28f5e87e675af4ac
birukwubishet <birukwubishet4560@gmail.com>
2022-04-09 11:22:27 -0700

    update commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = num1 + 1 ; num2 <= '9'; num2++)
+		{
+			if (num1 == num2)
+				continue;
+			else
+			{
+				putchar(num1);
+				putchar(num2);
+			}
+			if (num1 < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
