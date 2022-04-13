52ac04fa62da8466e16109de09ac596399f148f8
birukwubishet <birukwubishet4560@gmail.com>
2022-04-13 10:59:28 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_sign - Determines wether an intiger is positive, negative or zero
+ * @n: the number to be checked.
+ * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
+ */
+int print_sign(int n)
+
+	
+
+	
+
+
